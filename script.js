1f5a27480a29600ca3c09d165067cc0471b03597
PrimeTV <foxend@outlook.com>
2025-04-24 17:38:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const banner = document.getElementById("banner");
 
 // Lista com 6 imagens de banners
 
-
 let index = 0;
 
 // Função para trocar o banner
@@ -48,9 +47,40 @@ function loadVideo() {
 }
 
 
-function assinar(plano) {
-  alert(`Você escolheu o Plano ${plano}. Um atendente entrará em contato.`);
-  // Aqui você pode integrar com um sistema de pagamento ou WhatsApp
-  // window.location.href = "https://wa.me/5511950407835 ";
+const notificacoes = [
+  { nome: "Carlos Pavanato", mensagem: "✔ @Carlos Prado, acabou de Assinar um Plano Mensal Agora!" },
+  { nome: "Ana12 Ribeiro", mensagem: "📺 @Ana Julia12, Assinou o Pacote Família com 4 telas! Agora!" },
+  { nome: "Rafael Silva33", mensagem: "✔ @Rafael, Assinou o combo Esportes + Filmes! há 5 Minutos" },
+  { nome: "Juliana Costa53", mensagem: "💳 @Juliana22, Assinou o plano Anual com 20% Desconto! há 1 minuto" },
+  { nome: "Marcos Montenegro", mensagem: "✔ @Marcos, Ativou 2 Meses Grátis no Plano Anual Agora!" },
+  { nome: "Fernanda Lima034", mensagem: "✔ @Fernanda, acabou de Assinar um Plano Tri-Mestral! Agora!" },
+  { nome: "Eduardo Machado", mensagem: "🎁 @Eduardo32, Recebeu 3 Horas de Teste Gratuito! há 30 Segundos" },
+  { nome: "Camila Oliveira", mensagem: "📦 @Camila, Assinou o Plano Mensal Premium Agora!!" },
+  { nome: "João Souza", mensagem: "🚀 @João, Assinou o plano Premium com +12Mil Canais! Agora!" },
+  { nome: "Larissa Rodrigues", mensagem: "🎬 @Larissa, Assinou o Combo Canais TV +Novelas e Séries ilimitadas! Agora!" }
+];
+
+function mostrarNotificacaoPersonalizada() {
+  const aleatorio = notificacoes[Math.floor(Math.random() * notificacoes.length)];
+  const container = document.getElementById("compra-notificacao");
+  const texto = document.getElementById("texto-notificacao");
+
+  texto.innerText = aleatorio.mensagem;
+  container.classList.add("visivel");
+
+  setTimeout(() => {
+    container.classList.remove("visivel");
+  }, 5000);
 }
+
+// Exibe uma notificação personalizada a cada 15 segundos
+setInterval(() => {
+  mostrarNotificacaoPersonalizada();
+}, 12000);
+
+// Primeira notificação após 2 segundos
+setTimeout(() => {
+  mostrarNotificacaoPersonalizada();
+}, 2000);
+
 
